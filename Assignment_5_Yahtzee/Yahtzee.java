87fee728d07afd8303fe047dec358d25c14c1945
pedja981 <pedja981@users.noreply.github.com>
2016-05-04 08:18:16 +0200

    Done

--- Assignment_5_Yahtzee/Yahtzee.java
+++ Assignment_5_Yahtzee/Yahtzee.java
@@ -7,6 +7,9 @@
 import acm.io.*;
 import acm.program.*;
 import acm.util.*;
+import java.awt.event.*;
+import java.util.*;
+
 
 public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 	
@@ -56,7 +59,7 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 			} 
 			
 			//checkForCategory();
-			if (checkForCategory()) {
+			if (checkForCategory(dice, category)) {
 				turnScore = calculateScore(dice);
 				score[i-1][category-1] = turnScore;
 				score[i-1][16] += turnScore;
@@ -124,11 +127,104 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 		return arr;
 	}*/
 	
-	private boolean checkForCategory() {
+	/*private boolean checkForCategory() {
 		if (YahtzeeMagicStub.checkCategory(dice, category)) {
 			return true;
 		} else {return false;}
+	}*/
+	
+	
+	
+	
+	
+
+	/* Pre-condition: The player has finished rolling the dice and selects a category. 
+	 * This method returns true if the selected category matches 
+	 * to the actual category correctly, and false if it does not match. */
+	private boolean checkForCategory(int[] dice, int category) {
+		boolean categoryMatch = false;
+		if(category >= ONES && category <= SIXES || category == CHANCE) {
+			categoryMatch = true;
+		}
+		else {
+			
+			//creates an array for each possible dice value (1-6)
+			ArrayList <Integer> ones = new ArrayList<Integer>();  
+			ArrayList <Integer> twos = new ArrayList<Integer>(); 
+			ArrayList <Integer> threes = new ArrayList<Integer>(); 
+			ArrayList <Integer> fours = new ArrayList<Integer>(); 
+			ArrayList <Integer> fives = new ArrayList<Integer>(); 
+			ArrayList <Integer> sixes = new ArrayList<Integer>();
+			
+			/*goes through each rolled die and puts 1 as a place-holder into the appropriate ArrayList
+			* e.g. if the first die value is 1, then 1 is added to the ones ArrayList or
+			* if the second die value is 5, then 1 is added to the fives ArrayList*/
+			for(int i = 0; i < N_DICE; i++) {
+				if(dice[i] == 1) {
+					ones.add(1);
+				}
+				else if(dice[i] == 2) {
+					twos.add(1);
+				}
+				else if(dice[i] == 3) {
+					threes.add(1);
+				}
+				else if(dice[i] == 4) {
+					fours.add(1);
+				}
+				else if(dice[i] == 5) {
+					fives.add(1);
+				}
+				else if(dice[i] == 6) {
+					sixes.add(1);
+				}
+			}
+			if(category == THREE_OF_A_KIND) {
+				if(ones.size() >= 3 || twos.size() >= 3 || threes.size() >= 3 || fours.size() >= 3 || fives.size() >= 3 || sixes.size() >= 3) {
+					categoryMatch = true;
+				}
+			}	
+			else if(category == FOUR_OF_A_KIND) { 
+				if(ones.size() >= 4 || twos.size() >= 4 || threes.size() >= 4 || fours.size() >= 4 || fives.size() >= 4 || sixes.size() >= 4) {
+					categoryMatch = true;
+				}
+			}
+			else if(category == YAHTZEE) {
+				if(ones.size() == 5 || twos.size() == 5 || threes.size() == 5 || fours.size() == 5 || fives.size() == 5 || sixes.size() == 5) {
+					categoryMatch = true;
+				}
+			}
+			else if(category == FULL_HOUSE) {
+				if(ones.size() == 3 || twos.size() == 3 || threes.size() == 3 || fours.size() == 3 || fives.size() == 3 || sixes.size() == 3) {
+					if(ones.size() == 2 || twos.size() == 2 || threes.size() == 2 || fours.size() == 2 || fives.size() == 2 || sixes.size() == 2) {
+						categoryMatch = true;
+					}
+				}
+			}	
+			else if(category == LARGE_STRAIGHT) { 
+				if(ones.size() == 1 && twos.size() == 1 && threes.size() == 1 && fours.size() == 1 && fives.size() == 1){
+					categoryMatch = true;
+				}
+				else if(twos.size() == 1 && threes.size() == 1 && fours.size() == 1 && fives.size() == 1 && sixes.size() == 1) {
+					categoryMatch = true;
+				}
+			}
+			else if(category == SMALL_STRAIGHT) { 
+				if(ones.size() >= 1 && twos.size() >= 1 && threes.size() >= 1 && fours.size() >= 1) {
+					categoryMatch = true;
+				}
+				else if(twos.size() >= 1 && threes.size() >= 1 && fours.size() >= 1 && fives.size() >= 1) {
+					categoryMatch = true;
+				}
+				else if(threes.size() >= 1 && fours.size() >= 1 && fives.size() >= 1 && sixes.size() >= 1) {
+					categoryMatch = true;
+				}
+			}
+		}
+		return categoryMatch;
 	}
+	
+	
 	
 	
 	private int calculateScore (int[] dice) {
@@ -217,10 +313,7 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 		return turnScore;
 
 	}
-	
-	
-	
-	
+		
 	
 	// initializing the 2-dimensional score array
 	private int[][] initScore() {
@@ -237,7 +330,7 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 /* Private instance variables */
 	private int category;
 	private int[] dice;
-	private int[][] score;
+	private int[][] score; // score array for every player
 	private int turnScore; // score per turn - calculated or 0
 	private int nPlayers;
 	private String[] playerNames;
@@ -245,69 +338,3 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 	private RandomGenerator rgen = new RandomGenerator();
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
