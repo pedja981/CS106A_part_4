5dfe3db3f39c30d5904c24e33b6d34276fc1b2cd
pedja981 <pedja981@users.noreply.github.com>
2016-05-04 02:43:28 +0200

    Continuing rolling dice done

--- Assignment_5_Yahtzee/Yahtzee.java
+++ Assignment_5_Yahtzee/Yahtzee.java
@@ -27,22 +27,59 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 
 	private void playGame() {
 		display.waitForPlayerToClickRoll(nPlayers);
-		int[] firstRoll = new int[N_DICE];
-		rollDice(firstRoll, N_DICE);
-		display.displayDice(firstRoll);
+		rollDiceThreeTimes();
 		
-		// display.displayDice[N_DICE];
-		// display.displayDice[5];
-		/* You fill this in */
 	}
 	
-	// Method for for rolling the dice
-	private int[] rollDice(int[] arr, int n) {
+	private void rollDiceThreeTimes(){
+		
+		int[] dice = new int[N_DICE];
+		firstRollDice(dice, N_DICE);
+		display.displayDice(dice);
+
+		checkUserChoice(dice, N_DICE);
+		display.displayDice(dice);
+		
+		checkUserChoice(dice, N_DICE);
+		display.displayDice(dice);
+	}
+	
+	// Method for for first rolling of the dice
+	private int[] firstRollDice(int[] arr, int n) {
 		for (int i=0; i<n; i++) {
 			arr[i] = rgen.nextInt(1, 6);
 		}
 		return arr;
 	}
+	
+	/*
+	 * Method for second and third rolling of the dice
+	 * Checking the user's choice, and roll 
+	 * (random generate the values) for the dice
+	 * 
+	 */
+	private int[] checkUserChoice(int[] arr, int n) {
+		display.waitForPlayerToSelectDice();
+		for (int i=0; i<n; i++) {
+			if (display.isDieSelected(i)) {
+				arr[i] = rgen.nextInt(1, 6);
+			}
+		}
+		return arr;
+	}
+	
+	
+	/*private int[] rollOneDice(int[] arr, int i) {
+		arr[i] = rgen.nextInt(1, 6);
+		return arr;
+	}*/
+	
+	
+	
+
+	
+	
+	
 	
 		
 /* Private instance variables */
@@ -52,3 +89,69 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 	private RandomGenerator rgen = new RandomGenerator();
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
